872ef8ed2cac688de9ffee2cea02cc701df7f1a3
Figby222 <figby222@outlook.com>
2024-09-05 00:00:25 -0700

    Render usersList in usersListGet() controller

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -3,7 +3,7 @@ import db from "../db/queries.mjs";
 async function usersListGet(req, res) {
     const usernames = await db.getAllUsernames();
     console.log("Usernames: ", usernames);
-    res.send("Usernames" + usernames.map(user => user.username).join(", "));
+    res.render("usersList", { title: "Usernames", users: usernames });
 }
 
 export { usersListGet }
