ec5e497b0d1b5fb67fd724a449ef15b637f1f536
Figby222 <figby222@outlook.com>
2024-09-04 19:00:39 -0700

    Add pool.mjs

---
+++ db/pool.mjs
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+import "dotenv/config";
+
+export default new Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+});
