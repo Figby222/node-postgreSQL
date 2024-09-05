9f45b6fa556f940af5b5feacb983bf5dbfa11c34
Figby222 <figby222@outlook.com>
2024-09-05 00:39:42 -0700

    Add logic to search for subString in usersListGet
    Add logic to search for subString in
    usersListGet() controller

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,6 +1,12 @@
 import db from "../db/queries.mjs";
 
 async function usersListGet(req, res) {
+    if (req.query.search) {
+        const usernames = await db.searchUsername(req.query.search);
+        console.log("Usernames: ", usernames);
+        return res.render("usersList", { title: "Users Search", users: usernames });
+    }
+
     const usernames = await db.getAllUsernames();
     console.log("Usernames: ", usernames);
     res.render("usersList", { title: "Usernames", users: usernames });
