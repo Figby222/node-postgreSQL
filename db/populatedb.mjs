89b113102c6471f47e9155838641245bd87c4b28
Figby222 <figby222@outlook.com>
2024-09-04 21:06:56 -0700

    Add populatedb.mjs

---
+++ db/populatedb.mjs
@@ -0,0 +1,30 @@
+import pg from "pg";
+import "dotenv/config";
+
+const { Client } = pg;
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS usernames (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        username VARCHAR ( 255 )
+    );
+
+    INSERT INTO usernames (username)
+    VALUES
+        ('Bryan'),
+        ('Odin'),
+        ('Damon');
+`;
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+        connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+}
+
+main();
